4aaa4234a42441fe6a5a8bb9e4348f1dfe7a3b54
vincenteof <vincenteof@qq.com>
2019-01-22 21:46:27 +0800

    hop from 2 different position

--- seasoned/chap13/chap13.rkt
+++ seasoned/chap13/chap13.rkt
@@ -1,38 +1,34 @@
 #lang racket
 ;; Use the trick from previous chapter: avoid passing non-recur variable and hide function
 ;; We have new tricks: use `call/cc` to return value abruptly and promptly
-(define intersect
-  (lambda (set1 set2)
-    (letrec ((I (lambda (set)
-                  (cond
-                    ((null? set) '())
-                    ((member (car set) set2)
-                     (cons (car set)
-                           (I (cdr set))))
-                    (else (I (cdr set)))))))
-      (cond
-        ((null? set2) '())
-        (else (I set1))))))
-
-(intersect '(tomatoes and macaroni) '(macaroni and cheese))
-
+;; Here We remove all unnecessary logic
 (define intersectall
   (lambda (lset)
-    (call/cc
-     (lambda (hop)
-       (letrec
-           ((A (lambda (lset)
-                 (cond
-                   ((null? (car lset))
-                    (hop '()))
-                   ((null? (cdr lset))
-                    (car lset))
-                   (else
-                    (intersect (car lset)
-                               (A (cdr lset))))))))
-         (cond
-           ((null? lset) '())
-           (else (A lset))))))))
+    (let/cc hop
+      (letrec
+          ((A (lambda (lset)
+                (cond
+                  ((null? (car lset))
+                   (hop '()))
+                  ((null? (cdr lset))
+                   (car lset))
+                  (else (I (car lset)
+                           (A (cdr lset)))))))
+           (I (lambda (s1 s2)
+                (letrec
+                    ((J (lambda (s1)
+                          (cond
+                            ((null? s1) '()) ;; actually this case will be handled in outer logic
+                            ((member (car s1) s2)
+                             (cons (car s1)
+                                        (J (cdr s1))))
+                            (else (J (cdr s1)))))))
+                  (cond
+                    ((null? s2) (hop '()))
+                    (else (J s1)))))))
+        (cond
+          ((null? lset) '())
+          (else (A lset)))))))
 
 (intersectall '((3 mangos and) (3 kiwis and) (3 hamburgers)))
 (intersectall '((3 mangos and) () (3 hamburgers)))
