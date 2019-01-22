6d8132c8f30ba1dda848867306bbe220fdb508bb
vincenteof <vincenteof@qq.com>
2019-01-22 20:57:48 +0800

    using call/cc

--- seasoned/chap13/chap13.rkt
+++ seasoned/chap13/chap13.rkt
@@ -1,5 +1,6 @@
 #lang racket
 ;; Use the trick from previous chapter: avoid passing non-recur variable and hide function
+;; We have new tricks: use `call/cc` to return value abruptly and promptly
 (define intersect
   (lambda (set1 set2)
     (letrec ((I (lambda (set)
@@ -15,11 +16,21 @@
 
 (define intersectall
   (lambda (lset)
-    (cond
-      ((null? lset) '())
-      ((null? (cdr lset)) (car lset))
-      (else (intersect (car lset)
-                       (intersectall (cdr lset)))))))
+    (call/cc
+     (lambda (hop)
+       (letrec
+           ((A (lambda (lset)
+                 (cond
+                   ((null? (car lset))
+                    (hop '()))
+                   ((null? (cdr lset))
+                    (car lset))
+                   (else
+                    (intersect (car lset)
+                              (A (cdr lset))))))))
+         (cond
+           ((null? lset) '())
+           (else (A lset))))))))
 
 (intersectall '((3 mangos and) (3 kiwis and) (3 hamburgers)))
 (intersectall '((3 mangos and) () (3 hamburgers)))
